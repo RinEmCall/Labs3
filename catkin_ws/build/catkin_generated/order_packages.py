ae11866c7719a32196c747b4bb8854a1d6fb2cb1
RinEmCall <73193260+RinEmCall@users.noreply.github.com>
2020-12-30 01:19:17 +0300

    11

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/elijah/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/elijah/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/elijah/catkin_ws/devel;/opt/ros/melodic' != '' else []
